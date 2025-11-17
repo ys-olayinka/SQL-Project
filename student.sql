55964d4ee6a76b1bed5e705c5119833449209566
Yusuf Olayinka <yusufolayinka1014@gmail.com>
2025-11-17 01:04:34 +0000

    Update student.sql

--- student.sql
+++ student.sql
@@ -36,12 +36,12 @@ select * from StudentDetail;
 
 select * from Enrolment;
 
-/*Question 1*/
+/*Question 1 Display learners whose FirstForename begins with the letter ‘N’. Display FirstForename only. */
 SELECT FirstForename
 FROM StudentDetail
 WHERE FirstForename LIKE 'N%';
 
-/*Question 2*/
+/*Question 2 Display learners that start on or after 1 st August 2024. Display RefNo, Name fields, EnrolmentID and StartDate. */
 SELECT a.RefNo,
        a.Surname, 
 	   a.FirstForename,
@@ -51,7 +51,8 @@ FROM StudentDetail as a
 INNER JOIN Enrolment as b ON a.StudentDetailID = b.StudentDetailID
 WHERE b.StartDate >= '2024-08-01';
 
-/*Question 3*/ 
+/*Question 3 Display learners that are still on programme in 23/24 and past their end date. Display RefNo, Name fields, ExpectedEndDate and how
+many days past their end date they are. */ 
 SELECT 
     a.RefNo, 
     a.Surname, 
@@ -64,19 +65,20 @@ WHERE b.CompletionStatusID = 1 /* Assuming 1 is 'Continuing*/
 AND b.ExpectedEndDate < CURRENT_DATE
 AND a.AcademicYearID = '23/24';
   
-/*Question 4*/  
+/*Question 4 Display how many learners have not returned or are not still with us in 23/24 after withdrawing*/  
 SELECT COUNT(*) AS WithdrawnLearners
 FROM StudentDetail as a
 INNER JOIN Enrolment as b  ON a.StudentDetailID = b.StudentDetailID
 WHERE b.CompletionStatusID = 3 -- Assuming 3 is 'Withdrawn'
 AND a.AcademicYearID = '23/24';
 
-/*Question 5*/
+/*Question 5 Group the number of learners by Funding ID*/
 SELECT b.FundingID, COUNT(*) AS NumberOfLearners
 FROM Enrolment as b
 GROUP BY b.FundingID;
 
-/*Question 6*/
+/*Question 6 Bring back learners with a FundingID of 25 and ExpectedGLH of over 400, and the OfferingID of the most relevant enrolment based on
+completion status (1 being most relevant to 4 being the least). Display RefNo, Name fields, OfferingID and CompletionStatus */
 SELECT 
     a.RefNo, 
     a.Surname, 
@@ -89,7 +91,7 @@ WHERE b.FundingID = 25
 AND b.ExpectedGLH > 400
 ORDER BY b.CompletionStatusID ASC; /* Order by most relevant (1) to least (4)*/
 
-
+/* Window Function */
 SELECT 
     StudentDetailID,
     OfferingID,
@@ -101,7 +103,7 @@ WHERE FundingID = 25
 ORDER BY CompletionStatusID ASC;
 
 
-/*Question 7*/
+/*Question 7 Display learners who are showing as transferred but never started a new enrolment. Display Refno, Name fields and OfferingID */
 SELECT 
     a.RefNo, 
     a.Surname, 
